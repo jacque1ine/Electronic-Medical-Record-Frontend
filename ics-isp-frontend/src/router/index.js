9949adb1a80cbd12c4f8a25a349baa827ab1329e
Jacqueline Fung <fungjsl@gmail.com>
2022-05-27 12:46:37 -0400

    added calender view to index

--- ics-isp-frontend/src/router/index.js
+++ ics-isp-frontend/src/router/index.js
@@ -10,10 +10,12 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  }, 
+  {
+    path: '/calendar',
+    name: 'calendar',
+    component: () => import(/* webpackChunkName: "about" */ '../views/CalendarView.vue')
   }
 ]
 
