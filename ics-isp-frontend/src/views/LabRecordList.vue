0d0cd16dbdcb229689fb077ef69c6be8549de7bd
nikvettese <76953721+nikvettese@users.noreply.github.com>
2022-06-02 12:33:35 -0400

    Create LabRecordList.vue

---
+++ ics-isp-frontend/src/views/LabRecordList.vue
@@ -0,0 +1,28 @@
+<template>
+  <LabRecord :searchKeyword="searchKeyword"></LabRecord>
+</template>
+
+<script>
+
+import LabRecord from "@/components/LabRecord";
+
+export default {
+  name: 'PatientProfile',
+  components:{
+    LabRecord
+  },
+  data(){
+    return {
+      searchKeyword: ''
+    };
+  },
+  methods: {
+  },
+  async created(){
+
+  }
+};
+</script>
+
+<style>
+</style>
